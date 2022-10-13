3573b4a961292831c4b82e6b718925a7633a230a
leo <leonunes215@outlook.com>
2022-10-13 10:53:08 -0300

    ci: Add publish storybook workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, {configType}) => {
+    if (configType === 'PRODUCTION') {
+      configType.base = '/Ignite-lab-design-system/'
+    }
+
+    return config
   }
 }
